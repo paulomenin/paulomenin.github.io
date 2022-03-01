6c8ca36dc7b5ddc82183e975fcefd2a7bac2f96e
Paulo Menin <paulomenin@gmail.com>
2022-03-01 16:34:40 -0300

    Change logo

--- site/gatsby-config.js
+++ site/gatsby-config.js
@@ -1,73 +1,79 @@
 module.exports = {
-    siteMetadata: {
-        title: `Paulo Menin`,
-        author: {
-            name: `Paulo Menin`,
-            summary: `Software Engineer | Architect`,
-        },
-        description: `Paulo Menin's personal Blog and Portfolio`,
-        siteUrl: `https://github.com/paulomenin`,
-        social: {
-            github: `https://github.com/paulomenin`,
-            linkedin: `https://linkedin.com/in/paulo-henrique-menin`,
-        },
-        contact: {
-            email: `paulomenin@gmail.com`,
-        },
+  siteMetadata: {
+    title: `Paulo Menin`,
+    author: {
+      name: `Paulo Menin`,
+      summary: `Software Engineer | Architect`,
+    },
+    description: `Paulo Menin's personal Blog and Portfolio`,
+    siteUrl: `https://github.com/paulomenin`,
+    social: {
+      github: `https://github.com/paulomenin`,
+      linkedin: `https://linkedin.com/in/paulo-henrique-menin`,
+    },
+    contact: {
+      email: `paulomenin@gmail.com`,
+    },
+  },
+
+  plugins: [
+    `gatsby-plugin-image`,
+    `gatsby-plugin-postcss`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `posts`,
+        path: `${__dirname}/content/`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 630,
+            },
+          },
+          {
+            resolve: `gatsby-remark-responsive-iframe`,
+            options: {
+              wrapperStyle: `margin-bottom: 1.0725rem`,
+            },
+          },
+          `gatsby-remark-prismjs`,
+          `gatsby-remark-copy-linked-files`,
+          `gatsby-remark-smartypants`,
+          `gatsby-remark-reading-time`,
+        ],
+      },
+    },
+    `gatsby-transformer-sharp`,
+    {
+      resolve: `gatsby-plugin-sharp`,
+      options: {
+        icon: `src/images/logo.svg`,
+      },
     },
 
-    plugins: [
-        `gatsby-plugin-image`,
-        `gatsby-plugin-postcss`,
-        {
-            resolve: `gatsby-source-filesystem`,
-            options: {
-                name: `posts`,
-                path: `${__dirname}/content/`,
-            },
-        },
-        {
-            resolve: `gatsby-source-filesystem`,
-            options: {
-                name: `images`,
-                path: `${__dirname}/src/images`,
-            },
-        },
-        {
-            resolve: `gatsby-transformer-remark`,
-            options: {
-                plugins: [
-                    {
-                        resolve: `gatsby-remark-images`,
-                        options: {
-                            maxWidth: 630,
-                        },
-                    },
-                    {
-                        resolve: `gatsby-remark-responsive-iframe`,
-                        options: {
-                            wrapperStyle: `margin-bottom: 1.0725rem`,
-                        },
-                    },
-                    `gatsby-remark-prismjs`,
-                    `gatsby-remark-copy-linked-files`,
-                    `gatsby-remark-smartypants`,
-                    `gatsby-remark-reading-time`,
-                ],
-            },
-        },
-        `gatsby-transformer-sharp`,
-        `gatsby-plugin-sharp`,
-        // {
-        //   resolve: `gatsby-plugin-google-analytics`,
-        //   options: {
-        //     trackingId: `ADD YOUR TRACKING ID HERE`,
-        //   },
-        // },
-        {
-            resolve: `gatsby-plugin-feed`,
-            options: {
-                query: `
+    // {
+    //   resolve: `gatsby-plugin-google-analytics`,
+    //   options: {
+    //     trackingId: `ADD YOUR TRACKING ID HERE`,
+    //   },
+    // },
+    {
+      resolve: `gatsby-plugin-feed`,
+      options: {
+        query: `
           {
             site {
               siteMetadata {
@@ -79,26 +85,20 @@ module.exports = {
             }
           }
         `,
-                feeds: [
-                    {
-                        serialize: ({ query: { site, allMarkdownRemark } }) => {
-                            return allMarkdownRemark.nodes.map(node => {
-                                return Object.assign({}, node.frontmatter, {
-                                    description: node.excerpt,
-                                    date: node.frontmatter.date,
-                                    url:
-                                        site.siteMetadata.siteUrl +
-                                        node.fields.slug,
-                                    guid:
-                                        site.siteMetadata.siteUrl +
-                                        node.fields.slug,
-                                    custom_elements: [
-                                        { "content:encoded": node.html },
-                                    ],
-                                })
-                            })
-                        },
-                        query: `
+        feeds: [
+          {
+            serialize: ({ query: { site, allMarkdownRemark } }) => {
+              return allMarkdownRemark.nodes.map(node => {
+                return Object.assign({}, node.frontmatter, {
+                  description: node.excerpt,
+                  date: node.frontmatter.date,
+                  url: site.siteMetadata.siteUrl + node.fields.slug,
+                  guid: site.siteMetadata.siteUrl + node.fields.slug,
+                  custom_elements: [{ "content:encoded": node.html }],
+                })
+              })
+            },
+            query: `
               {
                 allMarkdownRemark(
                   sort: { order: DESC, fields: [frontmatter___date] },
@@ -117,29 +117,29 @@ module.exports = {
                 }
               }
             `,
-                        output: "/rss.xml",
-                        title: "Paulo Menin RSS Feed",
-                    },
-                ],
-            },
-        },
-        {
-            resolve: `gatsby-plugin-manifest`,
-            options: {
-                name: `Paulo Menin Blog`,
-                short_name: `Paulo Menin`,
-                start_url: `/`,
-                background_color: `#ffffff`,
-                // This will impact how browsers show your PWA/website
-                // https://css-tricks.com/meta-theme-color-and-trickery/
-                theme_color: `#663399`,
-                display: `minimal-ui`,
-                icon: `static/android-chrome-512x512.png`, // This path is relative to the root of the site.
-            },
-        },
-        `gatsby-plugin-react-helmet`,
-        // this (optional) plugin enables Progressive Web App + Offline functionality
-        // To learn more, visit: https://gatsby.dev/offline
-        // `gatsby-plugin-offline`,
-    ],
+            output: "/rss.xml",
+            title: "Paulo Menin RSS Feed",
+          },
+        ],
+      },
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Paulo Menin Blog`,
+        short_name: `Paulo Menin`,
+        start_url: `/`,
+        background_color: `#ffffff`,
+        // This will impact how browsers show your PWA/website
+        // https://css-tricks.com/meta-theme-color-and-trickery/
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/images/logo.svg`, // This path is relative to the root of the site.
+      },
+    },
+    `gatsby-plugin-react-helmet`,
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.dev/offline
+    // `gatsby-plugin-offline`,
+  ],
 }
--- site/src/components/header.js
+++ site/src/components/header.js
@@ -1,33 +1,40 @@
 import * as React from "react"
 import { Link } from "gatsby"
 import { StaticImage } from "gatsby-plugin-image"
-import { RssIcon } from '@heroicons/react/solid'
+import { RssIcon } from "@heroicons/react/solid"
 import Menu from "./menu"
 
 const Header = ({ title }) => {
   return (
-    <header className="
+    <header
+      className="
         card
         flex flex-col
         m-4 gap-2
-    ">
-        <div className="flex flex-row justify-between">
-            <Link className="font-serif font-bold text-2xl" to="/">
-                <StaticImage src="../../static/favicon-32x32.png" alt="logo" className="mr-3"/>
-                {title}
-            </Link>
+    "
+    >
+      <div className="flex flex-row justify-between">
+        <Link className="font-sans font-bold text-2xl ml-2" to="/">
+          <StaticImage
+            src="../images/logo.svg"
+            alt="Logo"
+            className="mr-3 w-[65px]"
+            placeholder="none"
+            backgroundColor="#ffffff"
+          />
+          {title}
+        </Link>
 
-            <div className="items-end text-gray-700 hover:text-black">
-                <Link to="/rss.xml" title="RSS">
-                    <RssIcon className="h-7 w-7" />
-                </Link>
-            </div>
+        <div className="items-end text-gray-700 hover:text-black">
+          <Link to="/rss.xml" title="RSS">
+            <RssIcon className="h-7 w-7" />
+          </Link>
         </div>
+      </div>
 
-        <div className="">
-            <Menu />
-        </div>
-
+      <div className="">
+        <Menu />
+      </div>
     </header>
   )
 }
