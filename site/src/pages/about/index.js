d1560a881d29599326ccd4d3fb63fafc33f55355
Paulo Menin <paulomenin@gmail.com>
2022-03-01 19:18:43 -0300

    Add tech stack icons

--- site/src/pages/about/index.js
+++ site/src/pages/about/index.js
@@ -1,17 +1,27 @@
 import * as React from "react"
-import {graphql } from "gatsby"
-
+import { graphql } from "gatsby"
 import Layout from "../../components/layout"
 import Seo from "../../components/seo"
+import TechIcons from "./techIcons"
 
 const AboutPage = ({ data, location }) => {
   const siteTitle = data.site.siteMetadata?.title || `Title`
   return (
     <Layout location={location} title={siteTitle}>
       <Seo title="About" />
-      <div className="flex justify-center align-middle">
-        <div className="card m-0 p-5">
-          <p>About me</p>
+      <div className="flex flex-col gap-4">
+        <div className="w-full card p-4">
+          <h2 className="mb-3">About me</h2>
+          <p>Description</p>
+        </div>
+
+        <div className="w-full card p-4">
+          <h2>Tech Stack</h2>
+          <p className="mb-3 mt-3">
+            These are some of the technologies that I had worked with in my
+            carrer.
+          </p>
+          <TechIcons />
         </div>
       </div>
     </Layout>
